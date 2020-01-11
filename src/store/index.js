8b14d87299ec58c02b9c5bc77616b400f4c47f1a
Sun.k <lovesunsixin@qq.com>
2020-01-11 22:50:52 +0800

    vue状态管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/store'
 
 Vue.use(Vuex)
 
+// vuex和缓存数据的同步
 export default new Vuex.Store({
   state: {
+    user: auth.getUser() // 从缓存中读的
   },
   mutations: {
+    // payload载荷中携带user
+    updateUser (state, payload) {
+      state.user = payload.user // 更新数据
+      auth.setUser() // 将数据同步设置到本地存储中
+    },
+    // 清除数据
+    clearUser (state) {
+      state.user = {}
+      auth.delUser() // 将缓存中的数据也清空
+    }
   },
   actions: {
   },
