040830bfbfba3e658a58b379bd42db2fd09f7cb3
Sun.k <lovesunsixin@qq.com>
2020-01-11 18:22:40 +0800

    使用Vant组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css' // 样式文件
+import Vant from 'vant' // 引入vant组件库
+
+Vue.use(Vant) // 一旦引入vant,可以在任意位置使用vant组件
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>主页</div>
+  <div>
+    主页
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
