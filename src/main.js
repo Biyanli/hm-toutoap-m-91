1a1d8c18b880db0eff2f5cfd1604719c3ecea9cb
Sun.k <lovesunsixin@qq.com>
2020-01-15 18:00:25 +0800

    修改vant主题蓝色

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './permission' // 经过权限处理之后的router
 import store from './store'
-import 'vant/lib/index.css' // 样式文件
+import 'vant/lib/index.less' // 样式文件 换成less
 import '@/styles/index.less' // 全局样式
 import Vant from 'vant' // 引入vant组件库
 import 'amfe-flexible'
