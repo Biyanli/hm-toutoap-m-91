adc0a42d5dfb8e8a03f7b66b8b92f7c312c270eb
Sun.k <lovesunsixin@qq.com>
2020-02-04 18:00:54 +0800

    搜索中心-历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -4,23 +4,29 @@
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
     <!-- 搜索框 -->
-    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
+    <van-search @search="onSearch" v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想搜索 -->
-    <van-cell-group class="suggest-box"  v-if="q">
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
-    <div class="history-box" v-else>
+    <!-- 只有当有历史记录的时候才显示整个历史记录的盒子 -->
+    <div class="history-box" v-else-if="historyList.length">
       <div class="head">
         <span>历史记录</span>
-        <van-icon name="delete"></van-icon>
+        <!-- 清空所有历史记录 -->
+        <van-icon @click="clear" name="delete"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        <!-- 循环生成历史记录 item是惟一的 历史记录不应该重复 -->
+        <van-cell @click="toResult(item)" v-for="(item,index) in historyList" :key="item">
+          <!-- item是搜索的关键字 -->
+          <a class="word_btn">{{item}}</a>
+          <!-- 给删除按钮注册删除事件 -->
+          <!-- 事件修饰符 事件名.stop 表示阻止了事件冒泡 -->
+          <van-icon @click.stop="delHistory(index)" class="close_btn" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -28,11 +34,45 @@
 </template>
 
 <script>
+const key = 'hm-91-toutiao-history' // 用来当做存储本地历史记录的key
 export default {
   name: 'search',
   data () {
     return {
-      q: ''
+      q: '', // 查询内容
+      historyList: [] // 存放历史记录的内容
+    }
+  },
+  // 读取数据
+  created () {
+    this.historyList = JSON.parse(localStorage.getItem(key) || '[]') // 读取本地化的内容
+  },
+  methods: {
+    // 删除对应的历史记录
+    delHistory (index) {
+      this.historyList.splice(index, 1) // 删除对应的历史记录
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
+    },
+    // 跳到搜索结果页面
+    toResult (text) {
+      this.$router.push({ path: '/search/result', query: { q: text } }) // 直接跳转到搜索结果页面
+    },
+    // 清空所有历史
+    clear () {
+      this.historyList = [] // 清空所有历史
+      localStorage.setItem(key, '[]') // 写入缓存
+    },
+    // 什么时候触发 回车/虚拟键盘搜索
+    onSearch () {
+      // 跳转到搜索结果之前 应该把当前的搜索关键字 写入到历史记录
+      if (!this.q) return // 如果q为空 没有必要往下走
+      // 如果不为空，要将this.q加入到历史记录中
+      let obj = new Set(this.historyList) // 生成一个set变量 set对象自动去重
+      obj.add(this.q)
+      this.historyList = Array.from(obj) // 将set转回数组
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
+      // 也应该去搜索结果页面 而且也要携带参数
+      this.$router.push({ path: '/search/result', query: { q: this.q } }) // 直接跳转到搜索结果页面
     }
   }
 }
@@ -41,32 +81,32 @@ export default {
 <style lang='less' scoped>
 .history-box {
   padding: 0 20px;
-  .head{
+  .head {
     line-height: 36px;
     color: #999;
-    .van-icon{
+    .van-icon {
       font-size: 16px;
       float: right;
-      margin-top: 10px;;
+      margin-top: 10px;
     }
   }
-  .van-cell{
+  .van-cell {
     padding: 10px 0;
   }
-  .word_btn{
-    color:#3296fa;
+  .word_btn {
+    color: #3296fa;
   }
-  .close_btn{
-    margin-top:5px;
+  .close_btn {
+    margin-top: 5px;
     color: #999;
   }
 }
-.suggest-box{
-  /deep/ .van-cell{
+.suggest-box {
+  /deep/ .van-cell {
     padding: 10px 20px;
     color: #999;
-    p{
-      span{
+    p {
+      span {
         color: red;
       }
     }
