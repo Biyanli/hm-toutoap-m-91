56163c5230467f46def503981b824cb08b67bf86
Sun.k <lovesunsixin@qq.com>
2020-02-04 16:06:19 +0800

    删除我的频道功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -24,7 +24,7 @@
     <van-action-sheet :round="false" title="编辑频道" v-model="showChannelEdit">
       <!-- 放置频道编辑组件 -->
       <!-- 父组件监听选择频道事件 -->
-      <channel-edit :activeIndex="activeIndex" @selectChannel="selectChannel" :channels="channels"></channel-edit>
+      <channel-edit @delChannel="delChannel" :activeIndex="activeIndex" @selectChannel="selectChannel" :channels="channels"></channel-edit>
     </van-action-sheet>
   </div>
 </template>
@@ -32,7 +32,7 @@
 <script>
 import ArticleList from './components/article-list'
 import MoreAction from './components/more-action'
-import { getMyChannels } from '@/api/channels'
+import { getMyChannels, delChannel } from '@/api/channels'
 import { disLikeArticle, reportArticle } from '@/api/article'
 import eventBus from '@/utils/eventBus'
 import ChannelEdit from './components/channel-edit'
@@ -54,6 +54,24 @@ export default {
     ChannelEdit
   },
   methods: {
+    // 删除频道的方法
+    async delChannel (id) {
+      try {
+        await delChannel(id) // 表示删除数据成功
+        // 要移除自身data中channels的数据
+        let index = this.channels.findIndex(item => item.id === id) // 找到删除的索引
+        if (index <= this.activeIndex) {
+          // 如果删除的频道在当前激活频道之前或者就是当前激活频道 要将激活索引往前挪一下
+          this.activeIndex = this.activeIndex - 1
+        }
+        if (index > -1) {
+          // 如果大于-1
+          this.channels.splice(index, 1) // 移除当前频道
+        }
+      } catch (error) {
+        this.$gnotify({ type: 'danger', message: '删除频道失败' })
+      }
+    },
     // 切换到对应的频道 关闭弹层
     selectChannel (id) {
       let index = this.channels.findIndex(item => item.id === id) // 获取切换频道的索引
