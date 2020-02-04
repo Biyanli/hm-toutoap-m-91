465613731dc35cf0d00ebfa3ea57ebbe8bc504a4
Sun.k <lovesunsixin@qq.com>
2020-02-04 16:54:20 +0800

    搜索关键词 和历史记录及联想搜索

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -4,15 +4,15 @@
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
     <!-- 搜索框 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
+    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想搜索 -->
-    <van-cell-group class="suggest-box" >
+    <van-cell-group class="suggest-box"  v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
-    <div class="history-box">
+    <div class="history-box" v-else>
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -29,7 +29,12 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: ''
+    }
+  }
 }
 </script>
 
