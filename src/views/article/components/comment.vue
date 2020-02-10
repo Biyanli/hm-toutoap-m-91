10bef047e19facc2ed8b9bb83b01f2adaec16fa9
Sun.k <lovesunsixin@qq.com>
2020-02-10 20:30:12 +0800

    文章详情-评论组件-评论与回复和个人中心-编辑资料-基础布局

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -26,14 +26,17 @@
         </div>
       </div>
     </van-list>
+    <!-- 提交模块 -->
     <div class="reply-container van-hairline--top">
-      <van-field v-model="value" placeholder="写评论...">
+      <!-- 给v-model加tirm，给评论内容清除空格 -->
+      <van-field v-model.trim ="value" placeholder="写评论...">
+        <!-- van-loading通过submiting状态来控制显示 -->
         <van-loading v-if="submiting" slot="button" type="spinner" size="16px"></van-loading>
-        <span class="submit" v-else slot="button">提交</span>
+        <span class="submit" @click="submit()" v-else slot="button">提交</span>
       </van-field>
     </div>
     <!-- 回复列表组件 -->
-    <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
+    <van-action-sheet @closed="reply.commentId=null"  v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
       <!-- 回复组件 -->
       <!-- immediate-check 是否在初始化时立即执行滚动位置检查 关闭了主动检查 第一次不会主动的去调用load事件对应的方法了 -->
       <van-list  :immediate-check="false" v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
@@ -53,7 +56,7 @@
 </template>
 
 <script>
-import { getComments } from '@/api/article' // 引入封装的获取评论的方法
+import { getComments, commentOrReply } from '@/api/article' // 引入封装的获取评论的方法 回复评论的方法
 export default {
   data () {
     return {
@@ -75,10 +78,50 @@ export default {
         offset: null, // 偏移量 获取评论的评论的分页依据 c
         list: [], // 用于存放 当前弹出的关于某个评论的回复列表的数据
         commentId: null // 用来存放 当前点击的评论Id
-      }
-    }
+      } }
   },
   methods: {
+    // 提交评论的方法
+    async submit () {
+      if (!this.value) return false // 表示如果当前评论内容为空就立刻返回
+      this.submiting = true // 将提交状态设置成true 控制用户单位时间内评论的数据次数
+      await this.$sleep() // 强制等待500 毫秒
+      try {
+      // 评论
+      // 一种是对文章进行评论
+      // 一种是对评论进行评论
+        // 如果不为空 继续
+        // 怎么样区分当前是对文章进行评论 还是对评论进行评论
+        // 两种方式 一种方式 通过 showReply的true/false
+        // 一种方式 通过 reply.commentId是存在
+        const data = await commentOrReply({
+        // this.reply.commentId 存在 就要对 评论进行评论  否则传文章ID
+          target: this.reply.commentId ? this.reply.commentId.toString() : this.$route.query.articleId, // 要么是文章id, 要么是 评论id
+          content: this.value, // 评论的内容
+          art_id: this.reply.commentId ? this.$route.query.articleId : null
+        // 如果此时对文章进行评论 art_id不能传 如果是对 评论进行评论 文章ID必须传
+        })
+        // 评论成功怎么处理 刷新页面 => 将评论的数据呈现到 视图上
+        // 需要知道加个哪个数组的最前面
+        if (this.reply.commentId) {
+          // 对评论进行评论
+          this.reply.list.unshift(data.new_obj) // 将数据加到 队首 评论的评论
+          // 如果对评论的进行评论之后, 应该找到该评论 并将 该评论的回复次数 +1
+          // 怎么找到 对应的评论
+          // 如果找到了 comment就是找到的对象 如果找不到 comment就是一个undefined
+          const comment = this.comments.find(item => item.com_id.toString() === this.reply.commentId.toString())
+          comment && comment.reply_count++ // 如果找到了 就对回复数据加1
+        } else {
+          // 对文章进行评论
+          this.comments.unshift(data.new_obj) // 文章评论
+        }
+        this.value = '' // 清空输入框
+      } catch (error) {
+        this.$gnotify({ type: 'danger', message: '评论失败' })
+      }
+      // 最后去关闭 状态
+      this.submiting = false // 关闭进度条
+    },
     openReply (commentId) {
       this.showReply = true
       // 进行若干操作
@@ -92,7 +135,7 @@ export default {
     },
     // // 获取评论的评论 也就是获取回复数据的方法
     async  getReply () {
-      // 加载逻辑
+    // 加载逻辑
       let data = await getComments({
         type: 'c', // c代表获取评论的评论
         offset: this.reply.offset, // 偏移量
@@ -103,12 +146,12 @@ export default {
       this.reply.loading = false // 评论的评论的加载状态关闭
       this.reply.finished = data.end_id === data.last_id // 如果last_id 等于end_id表示 没有数据可以再加载了
       if (!this.reply.finished) {
-        //  意味着有下一页的数据
+      //  意味着有下一页的数据
         this.reply.offset = data.last_id // 把当前页的最后一个id给偏移量 作为请求下一页的参数依据
       }
     },
     async onLoad () {
-      // 加载评论数据
+    // 加载评论数据
       let data = await getComments({
         type: 'a', // 获取类型
         offset: this.offset, // 偏移量
@@ -118,12 +161,14 @@ export default {
       this.loading = false // 关闭正在上拉加载状态
       this.finished = data.last_id === data.end_id // 如果当前页id等于整个评论最后一个id 表示 我们已经请求完了所有的数据
       if (!this.finished) {
-        // 表示 last_id 和 end_id不等 不等的话表示 还有下一页数据
+      // 表示 last_id 和 end_id不等 不等的话表示 还有下一页数据
         this.offset = data.last_id // 将last_Id设置成下一页的请求依据
       }
     }
+
   }
 }
+
 </script>
 
 <style lang='less' scoped>
--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,12 +1,77 @@
 <template>
   <div class="container">
-    <van-nav-bar left-arrow title="编辑资料" right-text="保存" @click-left="$router.back()"></van-nav-bar>
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存"></van-nav-bar>
+    <van-cell-group>
+      <!-- 头像 -->
+      <!-- 点击图片 显示选择图片的弹层 -->
+      <van-cell is-link title="头像" center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          @click="showPhoto = true"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <!-- 用户昵称 -->
+      <van-cell is-link title="名称" @click="showName = true" :value="user.name" />
+      <!-- 性别 -->
+      <van-cell is-link title="性别" @click="showGender = true" :value="user.gender === 0 ? '男':'女'" />
+      <!-- 生日 -->
+      <van-cell is-link title="生日" @click="showBirthday = true" :value="user.birthday" />
+    </van-cell-group>
+    <!-- 弹层组件 -->
+    <van-popup v-model="showPhoto" style="width:80%">
+      <!-- 内容 -->
+      <!-- 1.本地相册选择图片 -->
+      <!-- 2.拍照 -->
+      <van-cell is-link title="本地相册选择图片"></van-cell>
+      <van-cell is-link title="拍照"></van-cell>
+    </van-popup>
+    <!-- 弹层昵称 -->
+    <van-popup v-model="showName" style="width:80%">
+      <!-- 编辑用户昵称 双向绑定用户的昵称 -->
+      <van-field v-model="user.name" type="textarea" rows="4"></van-field>
+    </van-popup>
+    <!-- 性别选择上拉菜单 性别数据通过actions属性设置 -->
+    <van-action-sheet :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
+    <!-- 生日弹层 -->
+    <van-popup v-model="showBirthday" position="bottom">
+      <!-- 选择出生日期 出生日期应该小于现在时间 -->
+      <!-- type 表示当前的日期类型 年月日 -->
+      <van-datetime-picker
+        v-model="currentDate"
+        type="date"
+        :min-date="minDate"
+        :max-date="maxDate"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'profile'
+  name: 'profile',
+  data () {
+    return {
+      minDate: new Date(1900, 1, 1), // 最小时间
+      maxDate: new Date(), // 生日最大时间 永远小于等于当前时间
+      currentDate: new Date(), // 当前时间
+      showBirthday: false, // 是否显示生日弹层
+      showPhoto: false, // 是否显示选择头像弹层
+      showName: false, // 是否显示编辑昵称的弹层
+      showGender: false, // 是否显示性别选择的弹层
+      // 定义数据
+      user: {
+        name: '张三', // 用户昵称
+        gender: 1, // 男 0 女 1
+        birthday: '2019-08-08' // 默认生日
+      },
+      actions: [{ name: '男' }, { name: '女' }]
+    }
+  }
 }
 </script>
 
