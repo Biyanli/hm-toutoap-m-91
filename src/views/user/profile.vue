6961ca69be48d204361e175a1f2dc2cd17f55058
Sun.k <lovesunsixin@qq.com>
2020-02-10 20:53:05 +0800

    个人中心-编辑资料-基础布局（2）

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -30,13 +30,15 @@
       <van-cell is-link title="本地相册选择图片"></van-cell>
       <van-cell is-link title="拍照"></van-cell>
     </van-popup>
-    <!-- 弹层昵称 -->
-    <van-popup v-model="showName" style="width:80%">
+    <!-- 弹层昵称 关闭点击弹层 关闭功能 -->
+    <van-popup :close-on-click-overlay ="false" v-model="showName" style="width:80%">
       <!-- 编辑用户昵称 双向绑定用户的昵称 -->
-      <van-field v-model="user.name" type="textarea" rows="4"></van-field>
+      <van-field :error-message="nameMsg" v-model.trim="user.name" type="textarea" rows="4"></van-field>
+    <!-- 关闭按钮组件 -->
+    <van-button type="info" size="large" block @click="btnName">确定</van-button>
     </van-popup>
     <!-- 性别选择上拉菜单 性别数据通过actions属性设置 -->
-    <van-action-sheet :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
+    <van-action-sheet @select="selectItem" :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
     <!-- 生日弹层 -->
     <van-popup v-model="showBirthday" position="bottom">
       <!-- 选择出生日期 出生日期应该小于现在时间 -->
@@ -69,7 +71,27 @@ export default {
         gender: 1, // 男 0 女 1
         birthday: '2019-08-08' // 默认生日
       },
-      actions: [{ name: '男' }, { name: '女' }]
+      actions: [{ name: '男' }, { name: '女' }],
+      nameMsg: '' // 专门用来控制显示的错误信息
+    }
+  },
+  methods: {
+    // 绑定按钮点击事件
+    btnName () {
+      if (this.user.name.length < 1 || this.user.name.length > 7) {
+        // 如果长度小于1或者大于7，表示昵称不合规范
+        this.nameMsg = '您的用户名长度不符合1-7的要求'
+        return false // 不会继续往下执行
+      }
+      // 如果满足的话就会继续执行
+      this.nameMsg = '' // 先把提示消息清空
+      this.showName = false // 关闭弹层
+    },
+    // 当点击菜单项时，会触发该方法
+    selectItem (item) {
+      // console.log(item) // item就是选择的对象
+      this.user.gender = item.name === '男' ? 0 : 1 // 根据判断得到当前的性别
+      this.showGender = false // 关闭当前的弹层
     }
   }
 }
